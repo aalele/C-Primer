10c08a0d100d23cca64782041242610702d5db5a
allele <allele@zju.edu.cn>
2018-05-15 14:45:26 +0800

    init

---
+++ C02/C02/TypeConversion.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	unsigned u = 10, u2 = 42;
+	cout << u2 - u << endl << u - u2 << endl;
+
+	int i = 10, i2 = 42;
+	cout << i - u << endl << u - i;
+	return 0;
+}
